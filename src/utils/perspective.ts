716cc57a4060a6fac8c491e6eacbfc383481ea99
Mike Hepburn <eformat@gmail.com>
2026-05-06 17:02:09 +1000

    🔋make rhai workshops a perspective 🔋

---
+++ src/utils/perspective.ts
@@ -0,0 +1,7 @@
+import RhaiWorkshopIcon from '../components/RhaiWorkshopIcon';
+
+export const icon = { default: RhaiWorkshopIcon };
+
+export const getLandingPageURL = () => '/rhai-workshop';
+
+export const getImportRedirectURL = () => '/rhai-workshop';
